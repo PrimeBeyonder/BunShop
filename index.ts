422d51ee316fbd9c09f2cb3ed6f653c01d9bb35a
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2024-12-22 16:11:04 +0630

    about URL claas

--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
 const server = Bun.serve({
     port: 3000,
     fetch(request, server) {
-        return new Response("Hello World!");
+        const url = new URL(request.url);
+        console.log(url);
+        switch (request.url) {
+            case 'http://localhost:3000/home':
+                return new Response("Welcome to the Home" + request.url);
+            case 'http://localhost:3000/register':
+                return new Response("Welcome to the Register" + request.url);
+            default:
+                return new Response("Welcome to the Root" + request.url);
+        }
     }
 })
 console.log(server.port);
