9c8b186777b1bd431e8bfb503637aa9dd3c1a1da
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2024-12-22 16:15:30 +0630

    response the JSON

--- index.ts
+++ index.ts
@@ -1,9 +1,25 @@
+const product = {
+    code : 1001,
+    name: "Apple",
+}
+const resBody = JSON.stringify(product);
+console.log(resBody);
 const server = Bun.serve({
     port: 3000,
     fetch(request, server) {
         const url = new URL(request.url);
         console.log(url);
         switch (request.url) {
+            case "http://localhost:3000/products":
+                return new Response(
+                    resBody,
+                    {
+                        status: 200,
+                        headers: {
+                            "Content-Type": "application/json",
+                        }
+                    }
+                );
             case 'http://localhost:3000/home':
                 return new Response("Welcome to the Home" + request.url);
             case 'http://localhost:3000/register':
