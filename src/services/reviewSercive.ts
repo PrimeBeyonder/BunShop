0b7e23f07ef85b495a51a570d86b27414e7f4b25
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2025-01-08 20:09:14 +0630

    Review CRUD api

---
+++ src/services/reviewSercive.ts
@@ -0,0 +1,26 @@
+import { PrismaClient, Review } from '@prisma/client';
+import { AppError} from "../utils/ errorHandler.ts";
+
+const prisma = new PrismaClient();
+
+export const createReview = async (data: Omit<Review, 'id' | 'createdAt' | 'updatedAt'>): Promise<Review> => {
+    return prisma.review.create({ data });
+};
+
+export const getReviewsByProductId = async (productId: number): Promise<Review[]> => {
+    return prisma.review.findMany({
+        where: { productId },
+        include: { user: { select: { id: true, name: true } } }
+    });
+};
+
+export const updateReview = async (id: number, data: Partial<Omit<Review, 'id' | 'createdAt' | 'updatedAt'>>): Promise<Review> => {
+    return prisma.review.update({
+        where: { id },
+        data
+    })
+}
+
+export const deleteReview = async (id: number): Promise<void> => {
+    await prisma.review.delete({ where: { id } });
+}
