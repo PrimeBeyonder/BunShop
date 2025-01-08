ebc9ec6c30306a2cb82fd82a380f39f766106613
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2025-01-08 19:38:50 +0630

    category service

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -3,22 +3,30 @@ import { AppError} from "../utils/ errorHandler.ts";
 
 const prisma = new PrismaClient();
 
-export const createCategory =async (data: Omit<Category, 'id' | 'createdAt' | 'updatedAt'>): Promise<Category> => {
-    return prisma.category.create({data});
+export const createCategory = async (data: Omit<Category, 'id' | 'createdAt' | 'updatedAt'>): Promise<Category> => {
+    return prisma.category.create({ data });
 };
 
 export const getCategoryById = async (id: number): Promise<Category | null> => {
     return prisma.category.findUnique({
-            where: {id: id},
-            include: { subCategories: true, products: true }
-        });
-}
+        where: { id },
+        include: { subCategories: true, products: true }
+    });
+};
+
+export const getAllCategories = async (): Promise<Category[]> => {
+    return prisma.category.findMany({
+        include: { subCategories: true }
+    });
+};
+
 export const updateCategory = async (id: number, data: Partial<Omit<Category, 'id' | 'createdAt' | 'updatedAt'>>): Promise<Category> => {
     return prisma.category.update({
         where: { id },
         data
     });
 };
+
 export const deleteCategory = async (id: number): Promise<void> => {
     const category = await prisma.category.findUnique({
         where: { id },
