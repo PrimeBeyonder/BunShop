cf5d2576cd02b6482e699b46378f7db869fcab3b
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2025-01-10 21:59:11 +0630

    use new route

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import { userRoutes } from './routes/user';
 import { productRoutes} from "./routes/products.ts";
 import logger from './utils/logger';
 import {orderRoutes} from "./routes/order.ts";
+import {categoryRoutes} from "./routes/category.ts";
+import {tagRoutes} from "./routes/tag.ts";
+import {reviewRoutes} from "./routes/review.ts";
 
 const app = new Elysia();
 
 app.use(userRoutes);
 app.use(productRoutes);
 app.use(orderRoutes);
+app.use(categoryRoutes);
+app.use(tagRoutes);
+app.use(reviewRoutes);
 
 app.get('/', () => 'Welcome to BunShop API!');
 
